61717e3dfd7a6a1d181c08798c2f1659778c7700
parvathavarthni <parvatha.varthni@crystaldelta.com>
2020-01-03 23:22:26 +0530

    Modifications

--- src/test/java/cucumbermain/Cucucmbermain.java
+++ src/test/java/cucumbermain/Cucucmbermain.java
@@ -13,31 +13,28 @@ import com.vimalselvam.cucumber.listener.Reporter;
 import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 import cucumber.api.CucumberOptions;
-	public class Cucucmbermain {
 	@RunWith(Cucumber.class)
 	@CucumberOptions(
 	features ="Features",
-	glue = {"Screens"}
+	glue = {"library.Loree_functionalities.java"}
 	
 	)
-		 public class Cucumber_Main extends AbstractTestNGCucumberTests {
-			//public class Cucumber_Main {
+	public class Cucucmbermain extends AbstractTestNGCucumberTests {
+		//public class Cucumber_Main {
 
-				@BeforeClass()
-				public void setup() throws Exception {
-					ExtentProperties extentProperties = ExtentProperties.INSTANCE;
-					extentProperties.setReportPath("output/myreport.html");
-					extentProperties.setProjectName("The Hive Automation");
-				}
-				@AfterClass()
-				public void teardown() throws UnirestException {
-					Reporter.loadXMLConfig(new File("src/test/resources/extent-config.xml"));
-					Reporter.setSystemInfo("user", System.getProperty("user.name"));
-					Reporter.setSystemInfo("os", System.getProperty("os.name"));
-					Reporter.setSystemInfo("Browser", "Google Chrome");
-					Reporter.setTestRunnerOutput("Sample test runner output message");
+			@BeforeClass()
+			public void setup() throws Exception {
+				ExtentProperties extentProperties = ExtentProperties.INSTANCE;
+				extentProperties.setReportPath("output/myreport.html");
+				extentProperties.setProjectName("The Hive Automation");
+			}
+			@AfterClass()
+			public void teardown() throws UnirestException {
+				Reporter.loadXMLConfig(new File("src/test/resources/extent-config.xml"));
+				Reporter.setSystemInfo("user", System.getProperty("user.name"));
+				Reporter.setSystemInfo("os", System.getProperty("os.name"));
+				Reporter.setSystemInfo("Browser", "Google Chrome");
+				Reporter.setTestRunnerOutput("Sample test runner output message");
 
-				}
-		 }
+			}
 	}
-
