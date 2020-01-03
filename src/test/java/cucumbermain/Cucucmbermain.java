2b58c35be2026ca3cdf2c4007d0cc21273174cb4
parvathavarthni <parvatha.varthni@crystaldelta.com>
2020-01-03 22:57:07 +0530

    Modiifed runner class

--- src/test/java/cucumbermain/Cucucmbermain.java
+++ src/test/java/cucumbermain/Cucucmbermain.java
@@ -1,16 +1,43 @@
 package cucumbermain;
 
+import java.io.File;
+
 import org.junit.runner.RunWith;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.vimalselvam.cucumber.listener.ExtentProperties;
+import com.vimalselvam.cucumber.listener.Reporter;
 
 import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 import cucumber.api.CucumberOptions;
 	public class Cucucmbermain {
 	@RunWith(Cucumber.class)
 	@CucumberOptions(
-	features ="Features"
+	features ="Features",
+	glue = {"Screens"}
 	
 	)
-	public class runner {
+		 public class Cucumber_Main extends AbstractTestNGCucumberTests {
+			//public class Cucumber_Main {
 
-}
+				@BeforeClass()
+				public void setup() throws Exception {
+					ExtentProperties extentProperties = ExtentProperties.INSTANCE;
+					extentProperties.setReportPath("output/myreport.html");
+					extentProperties.setProjectName("The Hive Automation");
+				}
+				@AfterClass()
+				public void teardown() throws UnirestException {
+					Reporter.loadXMLConfig(new File("src/test/resources/extent-config.xml"));
+					Reporter.setSystemInfo("user", System.getProperty("user.name"));
+					Reporter.setSystemInfo("os", System.getProperty("os.name"));
+					Reporter.setSystemInfo("Browser", "Google Chrome");
+					Reporter.setTestRunnerOutput("Sample test runner output message");
+
+				}
+		 }
 	}
+
