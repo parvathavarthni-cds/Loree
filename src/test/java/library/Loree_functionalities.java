628b13fa602b8d6c4c0dd348f57ccd8254e4bbcf
parvathavarthni <parvatha.varthni@crystaldelta.com>
2020-01-06 12:14:34 +0530

    Chanegd to firefox

--- src/test/java/library/Loree_functionalities.java
+++ src/test/java/library/Loree_functionalities.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -16,8 +17,8 @@ public static WebDriver driver;
 
 	public WebDriver LoginPage_verification(WebDriver driver,String url) throws InterruptedException
 	{
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
 		driver.get(url);
 		//driver.manage().window().setSize(new Dimension(1920, 1080));
 		driver.manage().window().maximize();
@@ -42,4 +43,5 @@ public static WebDriver driver;
 		return driver;
 	}
 }
+
 
